37d993207d5f8cbc8a7036bca83a2f20660c520f
othniel-agera <othniel.agera@flutterwavego.com>
2022-10-21 13:22:05 +0100

    Seeder

---
+++ seeder.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+// Load env
+dotenv.config({ path: "./config/config.env" });
+
+// Load models
+const Bootcamp = require("./models/bootcamp.model");
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI);
+
+// Read the JSON files
+const bootcamps = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+
+// Import into DB
+const importData = async () => {
+	try {
+		await Bootcamp.create(bootcamps);
+		console.log(`Data imported...`.green.inverse);
+		process.exit();
+	} catch (error) {
+		console.log(`${error}`.red);
+	}
+};
+
+// Delete from DB
+const deleteData = async () => {
+	try {
+		await Bootcamp.deleteMany();
+		console.log(`Data deleted...`.red.inverse);
+		process.exit();
+	} catch (error) {
+		console.log(error.red);
+	}
+};
+
+if (process.argv[2] === "-i") {
+	importData();
+} else if (process.argv[2] === "-d") {
+	deleteData();
+}
