aa7f8a81f88f80f60c6fd6894d2d9303969966cb
othniel-agera <othniel.agera@flutterwavego.com>
2022-10-23 07:22:13 +0100

    Loc

--- controllers/bootcamp.ctrl.js
+++ controllers/bootcamp.ctrl.js
@@ -1,6 +1,8 @@
 const Bootcamp = require("../models/bootcamp.model");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middlewares/async");
+const geocoder = require("../utils/geocoder");
+const { find } = require("../models/bootcamp.model");
 class BootcampController {
 	/**
 	 * @desc Fetch bootcamps
@@ -27,6 +29,37 @@ class BootcampController {
 			);
 		res.status(200).json({ success: true, data: bootcamp });
 	});
+
+	/**
+	 * @desc Fetch bootcamp within a radius
+	 * @route GET /api/vi/bootcamps/radius/:zipcode/:distance
+	 * @access Private
+	 */
+
+	fetchWithinRadius = asyncHandler(async (req, res, next) => {
+		const { zipcode, distance } = req.params;
+
+		//Get lat/lng from geocoder;
+
+		const loc = await geocoder.geocode(zipcode);
+		const lat = loc[0].latitude;
+		const lng = loc[0].longitude;
+
+		// Calc radius using radians
+		//Divide dist by radius of Earth
+		// Earth Radius = 3,963mi / 6,378km
+		const radius = distance / 3963;
+
+		const bootcamps = await Bootcamp.find({
+			location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+		});
+		res.status(200).json({
+			success: true,
+			count: bootcamps.length,
+			result: bootcamps,
+		});
+	});
+
 	/**
 	 * @desc Create bootcamps
 	 * @route POST /api/vi/bootcamps
--- routes/bootcamp.route.js
+++ routes/bootcamp.route.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const {
 	fetchAll,
 	fetch,
+	fetchWithinRadius,
 	create,
 	update,
 	destroy,
 } = require("../controllers/bootcamp.ctrl");
 
+router.route("/radius/:zipcode/:distance").get(fetchWithinRadius);
 router.route("/").get(fetchAll).post(create);
 router.route("/:id").get(fetch).put(update).delete(destroy);
 
