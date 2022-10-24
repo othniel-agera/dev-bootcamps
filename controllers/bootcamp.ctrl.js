7d9486acff80e1d719a5cd1b950cdd187f665045
othniel-agera <othniel.agera@flutterwavego.com>
2022-10-24 18:59:31 +0100

    Pagination

--- controllers/bootcamp.ctrl.js
+++ controllers/bootcamp.ctrl.js
@@ -10,9 +10,75 @@ class BootcampController {
 	 * @access Public
 	 */
 	fetchAll = asyncHandler(async (req, res, next) => {
-		const bootcamps = await Bootcamp.find({});
-		res.status(200).json({ success: true, data: bootcamps });
-		// ;
+		let query;
+
+		// Copy req.query
+		const reqQuery = { ...req.query };
+
+		// Fields to exclude
+		const removeFields = ["select", "sort", "page", "limit"];
+
+		// Loop over removeFields and delete them from reqQuery
+		removeFields.forEach((param) => delete reqQuery[param]);
+
+		// Create query string
+		let queryStr = JSON.stringify(reqQuery);
+
+		// Create operators ( $gt, $gte, etc)
+		queryStr = queryStr.replace(
+			/\b(gt|gte|lt|lte|in)\b/g,
+			(match) => `$${match}`
+		);
+
+		// Finding resource
+		query = Bootcamp.find(JSON.parse(queryStr));
+
+		// Select fields
+		if (req.query.select) {
+			console.log(`req.query.select`.split(","));
+			console.log(typeof req.query.select);
+			const fields = req.query.select.split(",").join(" ");
+			query = query.select(fields);
+		}
+
+		// Sort
+		if (req.query.sort) {
+			const sortBy = req.query.sort.split(",").join(" ");
+			query = query.sort(sortBy);
+		} else {
+			query = query.sort("-createdAt");
+		}
+
+		// Pagination
+		const page = parseInt(req.query.page, 10) || 1;
+		const limit = parseInt(req.query.limit, 10) || 25;
+		const startIndex = (page - 1) * limit;
+		const endIndex = page * limit;
+		const total = await Bootcamp.countDocuments();
+		query = query.skip(startIndex).limit(limit);
+
+		const bootcamps = await query.exec();
+
+		// Pagination result
+		const pagination = {};
+		if (endIndex < total) {
+			pagination.next = {
+				page: page + 1,
+				limit,
+			};
+		}
+		if (startIndex > 0) {
+			pagination.prev = {
+				page: page - 1,
+				limit,
+			};
+		}
+		res.status(200).json({
+			success: true,
+			count: bootcamps.length,
+			pagination,
+			data: bootcamps,
+		});
 	});
 
 	/**
@@ -20,7 +86,6 @@ class BootcampController {
 	 * @route GET /api/vi/bootcamps
 	 * @access Public
 	 */
-
 	fetch = asyncHandler(async (req, res, next) => {
 		const bootcamp = await Bootcamp.findById(req.params.id);
 		if (!bootcamp)
@@ -35,7 +100,6 @@ class BootcampController {
 	 * @route GET /api/vi/bootcamps/radius/:zipcode/:distance
 	 * @access Private
 	 */
-
 	fetchWithinRadius = asyncHandler(async (req, res, next) => {
 		const { zipcode, distance } = req.params;
 
