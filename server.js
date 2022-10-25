a8f63c7380dd3da7a3a34d0e7546ced658a84305
othniel-agera <othniel.agera@flutterwavego.com>
2022-10-25 17:57:41 +0100

    Send JWT in cookie

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
+const cookieParser = require("cookie-parser");
 const fileupload = require("express-fileupload");
 
 //Route files
@@ -22,6 +23,7 @@ const app = express();
 //Body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 if (process.env.NODE_ENV === "development") {
 	app.use(morgan("dev"));
