3a975846d620cd9794b7aac1c3893614d38f5a78
othniel-agera <othniel.agera@flutterwavego.com>
2022-10-26 08:38:05 +0100

    Bootcamp ownership

--- controllers/bootcamp.ctrl.js
+++ controllers/bootcamp.ctrl.js
@@ -6,7 +6,7 @@ const geocoder = require("../utils/geocoder");
 class BootcampController {
 	/**
 	 * @desc Fetch bootcamps
-	 * @route GET /api/vi/bootcamps
+	 * @route GET /api/v1/bootcamps
 	 * @access Public
 	 */
 	fetchAll = asyncHandler(async (req, res) => {
@@ -15,7 +15,7 @@ class BootcampController {
 
 	/**
 	 * @desc Fetch bootcamp
-	 * @route GET /api/vi/bootcamps
+	 * @route GET /api/v1/bootcamps
 	 * @access Public
 	 */
 	fetch = asyncHandler(async (req, res) => {
@@ -29,7 +29,7 @@ class BootcampController {
 
 	/**
 	 * @desc Fetch bootcamp within a radius
-	 * @route GET /api/vi/bootcamps/radius/:zipcode/:distance
+	 * @route GET /api/v1/bootcamps/radius/:zipcode/:distance
 	 * @access Private
 	 */
 	fetchWithinRadius = asyncHandler(async (req, res) => {
@@ -58,32 +58,63 @@ class BootcampController {
 
 	/**
 	 * @desc Create bootcamps
-	 * @route POST /api/vi/bootcamps
-	 * @access Public
+	 * @route POST /api/v1/bootcamps
+	 * @access Private
 	 */
 	create = asyncHandler(async (req, res) => {
+		// Add user to req.body
+		req.body.user = req.user;
+
+		// Check for published bootcamp
+		const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+		// If user not admin the can only add one boodcamp
+		if (publishedBootcamp && req.user.role !== "admin") {
+			return next(
+				new ErrorResponse(
+					`The user with Id ${req.user.id} has already published a bootcamp`,
+					400
+				)
+			);
+		}
+
 		const bootcamp = await Bootcamp.create(req.body);
 		res.status(201).json({ success: true, data: bootcamp });
 	});
 
 	/**
 	 * @desc Update bootcamp
-	 * @route PUT /api/vi/bootcamps/:id
-	 * @access Public
+	 * @route PUT /api/v1/bootcamps/:id
+	 * @access Private
 	 */
-	update = asyncHandler(async (req, res) => {
-		const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+	update = asyncHandler(async (req, res, next) => {
+		const bootcamp = await Bootcamp.findById(req.params.id);
+		if (!bootcamp)
+			return next(
+				new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+			);
+		// Make sure user is bootcamp owner
+		if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+			return next(
+				new ErrorResponse(
+					`User ${req.params.id} is not authorized to update this bootcamp`,
+					401
+				)
+			);
+		}
+
+		bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
 			new: true,
 			runValidators: true,
 		});
-		if (!bootcamp) res.status(404).json({ success: false });
+
 		res.status(202).json({ success: true, data: bootcamp });
 	});
 
 	/**
 	 * @desc Destroy bootcamp
-	 * @route PUT /api/vi/bootcamps/:id
-	 * @access Public
+	 * @route PUT /api/v1/bootcamps/:id
+	 * @access Private
 	 */
 	destroy = asyncHandler(async (req, res) => {
 		const bootcamp = await Bootcamp.findById(req.params.id);
@@ -91,13 +122,22 @@ class BootcampController {
 			return next(
 				new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
 			);
+		// Make sure user is bootcamp owner
+		if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+			return next(
+				new ErrorResponse(
+					`User ${req.params.id} is not authorized to update this bootcamp`,
+					401
+				)
+			);
+		}
 		bootcamp.remove();
 		res.status(202).json({ success: true, data: {} });
 	});
 
 	/**
 	 * @desc Upload photo for bootcamp
-	 * @route PUT /api/vi/bootcamps/:id/photo
+	 * @route PUT /api/v1/bootcamps/:id/photo
 	 * @access Private
 	 */
 	photoUpload = asyncHandler(async (req, res, next) => {
@@ -126,6 +166,15 @@ class BootcampController {
 		file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
 		await file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`);
 
+		// Make sure user is bootcamp owner
+		if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+			return next(
+				new ErrorResponse(
+					`User ${req.params.id} is not authorized to update this bootcamp`,
+					401
+				)
+			);
+		}
 		await Bootcamp.findByIdAndUpdate(req.params.id, {
 			photo: file.name,
 		});
